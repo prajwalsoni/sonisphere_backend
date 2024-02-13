dbd4c11ae4fdecc43f4eebc8e8705665b8af04c1
prajwalsoni <prajwalsonics@gmail.com>
2024-02-13 22:57:28 +0530

    Added RealTimeChat

--- src/main/java/com/soni/controller/RealTimeChat.java
+++ src/main/java/com/soni/controller/RealTimeChat.java
@@ -1,70 +1,70 @@
-//package com.soni.controller;
-//
-//import java.util.Iterator; 
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.messaging.handler.annotation.DestinationVariable;
-//import org.springframework.messaging.handler.annotation.Header;
-//import org.springframework.messaging.handler.annotation.MessageMapping;
-//import org.springframework.messaging.handler.annotation.Payload;
-//import org.springframework.messaging.handler.annotation.SendTo;
-//import org.springframework.messaging.simp.SimpMessagingTemplate;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import com.soni.exception.ChatException;
-//import com.soni.exception.UserException;
-//import com.soni.model.Chat;
-//import com.soni.model.Message;
-//import com.soni.model.User;
-//import com.soni.request.SendMessageRequest;
-//import com.soni.service.ChatService;
-//import com.soni.service.MessageService;
-//import com.soni.service.UserService;
-//
-//
-//
-//@RestController
-//public class RealTimeChat {
-//	
-//    @Autowired
-//    private SimpMessagingTemplate simpMessagingTemplate;
-//    
-//    @Autowired
-//    private UserService userService;
-//    
-//    @Autowired
-//    private MessageService messageService;
-//    
-//    @Autowired
-//    private ChatService chatService;
-//	  
-//	@MessageMapping("/chat/{groupId}")
-//	public Message sendToUser(
-//		@Payload Message message, 
-//		@DestinationVariable String groupId) 
-//		throws UserException, ChatException {	
-//
-//		simpMessagingTemplate.convertAndSendToUser(
-//			groupId, 
-//			"/private", 
-//			message
-//		);
-//		return message;
-//	}
-//	
-//	public User reciver(Chat chat,User reqUser) {
-//		Iterator<User> iterator = chat.getUsers().iterator();
-//
-//		User user1 = iterator.next(); // get the first user
-//		User user2 = iterator.next();
-//		
-//		if(user1.getId().equals(reqUser.getId())){
-//			return user2;
-//		}
-//		return user1;
-//	}
-//	
-//
-//	
-//
-//}
+package com.soni.controller;
+
+import java.util.Iterator; 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.soni.exception.ChatException;
+import com.soni.exception.UserException;
+import com.soni.model.Chat;
+import com.soni.model.Message;
+import com.soni.model.User;
+import com.soni.request.SendMessageRequest;
+import com.soni.service.ChatService;
+import com.soni.service.MessageService;
+import com.soni.service.UserService;
+
+
+
+@RestController
+public class RealTimeChat {
+	
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
+    
+    @Autowired
+    private UserService userService;
+    
+    @Autowired
+    private MessageService messageService;
+    
+    @Autowired
+    private ChatService chatService;
+	  
+	@MessageMapping("/chat/{groupId}")
+	public Message sendToUser(
+		@Payload Message message, 
+		@DestinationVariable String groupId) 
+		throws UserException, ChatException {	
+
+		simpMessagingTemplate.convertAndSendToUser(
+			groupId, 
+			"/private", 
+			message
+		);
+		return message;
+	}
+	
+	public User reciver(Chat chat,User reqUser) {
+		Iterator<User> iterator = chat.getUsers().iterator();
+
+		User user1 = iterator.next(); // get the first user
+		User user2 = iterator.next();
+		
+		if(user1.getId().equals(reqUser.getId())){
+			return user2;
+		}
+		return user1;
+	}
+	
+
+	
+
+}
