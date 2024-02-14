52bd39f454283e03c94c51e99b4cd43c71365fe7
prajwalsoni <prajwalsonics@gmail.com>
2024-02-14 11:26:26 +0530

    Added StoryException and UserException

--- src/main/java/com/soni/exception/StoryException.java
+++ src/main/java/com/soni/exception/StoryException.java
@@ -1,14 +1,14 @@
-//package com.soni.exception;
-//
-//
-//
-//public class StoryException extends Exception{
-//
-//	public StoryException() {
-//		// TODO Auto-generated constructor stub
-//	}
-//	
-//	public StoryException(String message) {
-//		super();
-//	}
-//}
+package com.soni.exception;
+
+
+
+public class StoryException extends Exception{
+
+	public StoryException() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public StoryException(String message) {
+		super();
+	}
+}
