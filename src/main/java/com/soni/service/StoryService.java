815041ced92416fc44eae79fafcb8ca74f18d941
prajwalsoni <prajwalsonics@gmail.com>
2024-02-14 11:44:57 +0530

    Added StoryService , StoryServiceImplemnetation ,UserService and UserServiceImplementation

--- src/main/java/com/soni/service/StoryService.java
+++ src/main/java/com/soni/service/StoryService.java
@@ -1,17 +1,17 @@
-//package com.soni.service;
-//
-//import java.util.List;
-//
-//
-//import com.soni.exception.StoryException;
-//import com.soni.exception.UserException;
-//import com.soni.model.Story;
-//
-//public interface StoryService {
-//
-//	public Story createStory(Story story,Integer userId) throws UserException;
-//	
-//	public List<Story> findStoryByUserId(Integer userId) throws UserException, StoryException;
-//	
-//	
-//}
+package com.soni.service;
+
+import java.util.List;
+
+
+import com.soni.exception.StoryException;
+import com.soni.exception.UserException;
+import com.soni.model.Story;
+
+public interface StoryService {
+
+	public Story createStory(Story story,Integer userId) throws UserException;
+	
+	public List<Story> findStoryByUserId(Integer userId) throws UserException, StoryException;
+	
+	
+}
--- src/main/java/com/soni/service/UserService.java
+++ src/main/java/com/soni/service/UserService.java
@@ -1,32 +1,32 @@
-//package com.soni.service;
-//
-//import java.util.List;
-//
-//import java.util.Set;
-//
-//import com.soni.exception.UserException;
-//import com.soni.model.User;
-//
-//public interface UserService {
-//
-//	public User findUserProfileByJwt(String jwt) throws UserException;
-//
-//	public User registerUser(User user) throws UserException;
-//
-//	public User findUserById(Integer id) throws UserException;
-//
-//	public User findUserByEmail(String email) throws UserException;
-//
-//	public String followUser(Integer reqUserId, Integer followUserId) throws UserException;
-//
-//	public List<User> findUsersByUserIds(List<Integer> userIds);
-//
-//	public Set<User> searchUser(String query) throws UserException;
-//
-//	public User updateUserDetails(User updatedUser, User existingUser) throws UserException;
-//	
-//	void updatePassword(User user, String newPassword);
-//	
-//    void sendPasswordResetEmail(User user);
-//
-//}
+package com.soni.service;
+
+import java.util.List;
+
+import java.util.Set;
+
+import com.soni.exception.UserException;
+import com.soni.model.User;
+
+public interface UserService {
+
+	public User findUserProfileByJwt(String jwt) throws UserException;
+
+	public User registerUser(User user) throws UserException;
+
+	public User findUserById(Integer id) throws UserException;
+
+	public User findUserByEmail(String email) throws UserException;
+
+	public String followUser(Integer reqUserId, Integer followUserId) throws UserException;
+
+	public List<User> findUsersByUserIds(List<Integer> userIds);
+
+	public Set<User> searchUser(String query) throws UserException;
+
+	public User updateUserDetails(User updatedUser, User existingUser) throws UserException;
+	
+	void updatePassword(User user, String newPassword);
+	
+    void sendPasswordResetEmail(User user);
+
+}
