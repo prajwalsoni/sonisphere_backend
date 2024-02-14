8705d8da510d89ed96689e5cc78b19bde7aef7c9
prajwalsoni <prajwalsonics@gmail.com>
2024-02-14 11:45:34 +0530

    Added ChatUtil and PostUtil

--- src/main/java/com/soni/utils/PostUtils.java
+++ src/main/java/com/soni/utils/PostUtils.java
@@ -1,39 +1,39 @@
-//package com.soni.utils;
-//
-//import java.util.List;
-//
-//import java.util.Set;
-//
-//import com.soni.model.Post;
-//import com.soni.model.User;
-//
-//public class PostUtils {
-//	
-//	public static boolean isLikedByReqUser(Post post, User reqUser) {
-//		
-//		Set<User> like=post.getLiked();
-//		
-//		for(User user:like) {
-//			if(user.getId()==reqUser.getId()) {
-//				return true;
-//			}
-//		}
-//		
-//		return false;
-//		
-//	}
-//	
-//	public static boolean isSaved(Post post,User user) {
-//		
-//		List<Post> reqUsersPost=user.getSavedPosts();
-//		
-//		for(Post item : reqUsersPost) {
-//			if(item.getId()==post.getId()) {
-//				return true;
-//			}
-//		}
-//		
-//		return false;
-//	}
-//
-//}
+package com.soni.utils;
+
+import java.util.List;
+
+import java.util.Set;
+
+import com.soni.model.Post;
+import com.soni.model.User;
+
+public class PostUtils {
+	
+	public static boolean isLikedByReqUser(Post post, User reqUser) {
+		
+		Set<User> like=post.getLiked();
+		
+		for(User user:like) {
+			if(user.getId()==reqUser.getId()) {
+				return true;
+			}
+		}
+		
+		return false;
+		
+	}
+	
+	public static boolean isSaved(Post post,User user) {
+		
+		List<Post> reqUsersPost=user.getSavedPosts();
+		
+		for(Post item : reqUsersPost) {
+			if(item.getId()==post.getId()) {
+				return true;
+			}
+		}
+		
+		return false;
+	}
+
+}
