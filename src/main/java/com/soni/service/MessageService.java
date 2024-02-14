1380ab523134263edfc584b6231dd81856ae52e2
prajwalsoni <prajwalsonics@gmail.com>
2024-02-14 11:41:17 +0530

    Added CustomerUserServiceImplementation MessageService and PasswordResetTokenService

--- src/main/java/com/soni/service/MessageService.java
+++ src/main/java/com/soni/service/MessageService.java
@@ -1,21 +1,21 @@
-//package com.soni.service;
-//
-//import java.util.List; 
-//
-//import com.soni.exception.ChatException;
-//import com.soni.exception.MessageException;
-//import com.soni.exception.UserException;
-//import com.soni.model.Message;
-//import com.soni.request.SendMessageRequest;
-//
-//public interface MessageService  {
-//	
-//	public Message sendMessage(SendMessageRequest req) throws UserException, ChatException;
-//	
-//	public List<Message> getChatsMessages(Integer chatId) throws ChatException;
-//	
-//	public Message findMessageById(Integer messageId) throws MessageException;
-//	
-//	public String deleteMessage(Integer messageId) throws MessageException;
-//
-//}
+package com.soni.service;
+
+import java.util.List; 
+
+import com.soni.exception.ChatException;
+import com.soni.exception.MessageException;
+import com.soni.exception.UserException;
+import com.soni.model.Message;
+import com.soni.request.SendMessageRequest;
+
+public interface MessageService  {
+	
+	public Message sendMessage(SendMessageRequest req) throws UserException, ChatException;
+	
+	public List<Message> getChatsMessages(Integer chatId) throws ChatException;
+	
+	public Message findMessageById(Integer messageId) throws MessageException;
+	
+	public String deleteMessage(Integer messageId) throws MessageException;
+
+}
--- src/main/java/com/soni/service/PasswordResetTokenService.java
+++ src/main/java/com/soni/service/PasswordResetTokenService.java
@@ -1,11 +1,11 @@
-//package com.soni.service;
-//
-//import com.soni.model.PasswordResetToken;
-//
-//public interface PasswordResetTokenService {
-//
-//	public PasswordResetToken findByToken(String token);
-//
-//	public void delete(PasswordResetToken resetToken);
-//
-//}
+package com.soni.service;
+
+import com.soni.model.PasswordResetToken;
+
+public interface PasswordResetTokenService {
+
+	public PasswordResetToken findByToken(String token);
+
+	public void delete(PasswordResetToken resetToken);
+
+}
