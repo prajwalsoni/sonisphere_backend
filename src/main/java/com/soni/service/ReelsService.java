be22323781f9bcb953c8eef0c5db8239dc54f37b
prajwalsoni <prajwalsonics@gmail.com>
2024-02-14 11:43:02 +0530

    Added PasswordResetTokenServiceImplementation ,PostService , PostServiceImplementation and ReelsService

--- src/main/java/com/soni/service/ReelsService.java
+++ src/main/java/com/soni/service/ReelsService.java
@@ -1,16 +1,16 @@
-//package com.soni.service;
-//
-//import java.util.List;
-//
-//
-//import com.soni.exception.UserException;
-//import com.soni.model.Reels;
-//import com.soni.model.User;
-//
-//public interface ReelsService {
-//	
-//	public Reels createReel(Reels reel,User user);
-//	public List<Reels> findAllReels();
-//	public List<Reels> findUsersReel(Integer userId) throws UserException;
-//
-//}
+package com.soni.service;
+
+import java.util.List;
+
+
+import com.soni.exception.UserException;
+import com.soni.model.Reels;
+import com.soni.model.User;
+
+public interface ReelsService {
+	
+	public Reels createReel(Reels reel,User user);
+	public List<Reels> findAllReels();
+	public List<Reels> findUsersReel(Integer userId) throws UserException;
+
+}
