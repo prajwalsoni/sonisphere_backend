03b04d9b7c442706c78fcc17fd3a9808de86d9fd
prajwalsoni <prajwalsonics@gmail.com>
2024-02-14 11:25:45 +0530

    Added MessageException and PostException

--- src/main/java/com/soni/exception/PostException.java
+++ src/main/java/com/soni/exception/PostException.java
@@ -1,13 +1,13 @@
-//package com.soni.exception;
-//
-//public class PostException extends Exception {
-//	
-//	public PostException() {
-//		// TODO Auto-generated constructor stub
-//	}
-//	
-//	public PostException(String message) {
-//		super(message);
-//	}
-//
-//}
+package com.soni.exception;
+
+public class PostException extends Exception {
+	
+	public PostException() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public PostException(String message) {
+		super(message);
+	}
+
+}
