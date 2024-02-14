90bba5aa2f3718bb665869e170a3ef5eeb901db7
prajwalsoni <prajwalsonics@gmail.com>
2024-02-14 11:22:32 +0530

    Added MessageDtoMapper and PostDtoMapper

--- src/main/java/com/soni/dto/mapper/PostDtoMapper.java
+++ src/main/java/com/soni/dto/mapper/PostDtoMapper.java
@@ -1,47 +1,47 @@
-//package com.soni.dto.mapper;
-//
-//import java.util.ArrayList; 
-//import java.util.List;
-//
-//import com.soni.dto.CommentDto;
-//import com.soni.dto.PostDto;
-//import com.soni.dto.UserDto;
-//import com.soni.model.Post;
-//import com.soni.model.User;
-//import com.soni.utils.PostUtils;
-//
-//public class PostDtoMapper {
-//	
-//	public static PostDto toPostDto(Post post,User user) {
-//		
-//		UserDto userDto=UserDtoMapper.userDTO(post.getUser());
-//		List<User> likedUsers=new ArrayList<>(post.getLiked());
-//		List<UserDto> userDtos=UserDtoMapper.userDTOS(likedUsers);
-//		List<CommentDto> comments=CommentDtoMapper.toCommentDtos(post.getComments());
-//		
-//		PostDto postDto=new PostDto();
-//		postDto.setCaption(post.getCaption());
-//		postDto.setCreatedAt(post.getCreatedAt());
-//		postDto.setId(post.getId());
-//		postDto.setImage(post.getImage());
-//		postDto.setUser(userDto);
-//		postDto.setLiked(userDtos);
-//		postDto.setComments(comments);
-//		postDto.setLikedByRequser(PostUtils.isLikedByReqUser(post, user));
-//		postDto.setSavedByRequser(PostUtils.isSaved(post, user));
-//		
-//		return postDto;
-//		
-//	}
-//	
-//	public static List<PostDto> toPostDtos(List<Post> posts, User user){
-//		List<PostDto> postDtos=new ArrayList<>();
-//		
-//		for(Post post:posts) {
-//			PostDto postDto=toPostDto(post,user);
-//			postDtos.add(postDto);
-//		}
-//		return postDtos;
-//	}
-//
-//}
+package com.soni.dto.mapper;
+
+import java.util.ArrayList; 
+import java.util.List;
+
+import com.soni.dto.CommentDto;
+import com.soni.dto.PostDto;
+import com.soni.dto.UserDto;
+import com.soni.model.Post;
+import com.soni.model.User;
+import com.soni.utils.PostUtils;
+
+public class PostDtoMapper {
+	
+	public static PostDto toPostDto(Post post,User user) {
+		
+		UserDto userDto=UserDtoMapper.userDTO(post.getUser());
+		List<User> likedUsers=new ArrayList<>(post.getLiked());
+		List<UserDto> userDtos=UserDtoMapper.userDTOS(likedUsers);
+		List<CommentDto> comments=CommentDtoMapper.toCommentDtos(post.getComments());
+		
+		PostDto postDto=new PostDto();
+		postDto.setCaption(post.getCaption());
+		postDto.setCreatedAt(post.getCreatedAt());
+		postDto.setId(post.getId());
+		postDto.setImage(post.getImage());
+		postDto.setUser(userDto);
+		postDto.setLiked(userDtos);
+		postDto.setComments(comments);
+		postDto.setLikedByRequser(PostUtils.isLikedByReqUser(post, user));
+		postDto.setSavedByRequser(PostUtils.isSaved(post, user));
+		
+		return postDto;
+		
+	}
+	
+	public static List<PostDto> toPostDtos(List<Post> posts, User user){
+		List<PostDto> postDtos=new ArrayList<>();
+		
+		for(Post post:posts) {
+			PostDto postDto=toPostDto(post,user);
+			postDtos.add(postDto);
+		}
+		return postDtos;
+	}
+
+}
