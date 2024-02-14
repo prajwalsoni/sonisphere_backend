412769f20c307a0588c48f6e1f9938b8f985b8e9
prajwalsoni <prajwalsonics@gmail.com>
2024-02-14 11:14:07 +0530

    Added ReelsDTO

--- src/main/java/com/soni/dto/ReelsDto.java
+++ src/main/java/com/soni/dto/ReelsDto.java
@@ -1,17 +1,17 @@
-//package com.soni.dto;
-//
-//import com.soni.model.User;
-//
-//import jakarta.persistence.ManyToOne;
-//import lombok.Data;
-//
-//@Data
-//public class ReelsDto {
-//	
-//	private Long id;
-//	private String title;
-//	private String video;
-//	
-//	private UserDto user;
-//
-//}
+package com.soni.dto;
+
+import com.soni.model.User;
+
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+@Data
+public class ReelsDto {
+	
+	private Long id;
+	private String title;
+	private String video;
+	
+	private UserDto user;
+
+}
