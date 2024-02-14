f02fb5654641ca8862634e88e174dcbf5e8ff784
prajwalsoni <prajwalsonics@gmail.com>
2024-02-14 11:31:04 +0530

    Added ChatRepo CommentRepo and MsgRepo

--- src/main/java/com/soni/repository/CommentRepository.java
+++ src/main/java/com/soni/repository/CommentRepository.java
@@ -1,10 +1,10 @@
-//package com.soni.repository;
-//
-//import org.springframework.data.jpa.repository.JpaRepository; 
-//
-//import com.soni.model.Comments;
-//
-//
-//public interface CommentRepository extends JpaRepository<Comments, Integer> {
-//
-//}
+package com.soni.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository; 
+
+import com.soni.model.Comments;
+
+
+public interface CommentRepository extends JpaRepository<Comments, Integer> {
+
+}
